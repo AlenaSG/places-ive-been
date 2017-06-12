7713c08a857e7e88c0e584569b2eafd96c684fc0
Alena Golovina and lincoln nguyen <alena@>
2017-06-12 13:20:58 -0700

    add html, scc, js files to the project, create a list of places and display it as well as places descriptions

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,60 @@
+//business logic
+function Contact(first, last) {
+  this.placeName = first;
+  this.cityName = last;
+}
+
+function Description(link, season, review) {
+  this.myLocation = "portland";
+  this.myLink = link;
+  this.season = season;
+  this.review = review;
+}
+
+Contact.prototype.fullPlaceName = function() {
+  return this.placeName + ", " + this.cityName;
+}
+
+Description.prototype.fullDescription = function() {
+  return this.myLocation + ", " + this.myLink + ", " + this.season + ", " + this.review;
+}
+
+function resetFields() {
+    $("input#new-place-name").val("");
+    $("input#new-city-name").val("");
+    $("input#new-location-name").val("");
+    $("input#new-link-name").val("");
+    $("input#new-season-name").val("");
+    $("input#new-review-name").val("");
+  }
+
+// user interface logic
 $(document).ready(function() {
-  $("form#pig-latin").submit(function(event) {
+  $("form#new-contact").submit(function(event) {
     event.preventDefault();
-    var stringToTranslate = [];
-    var str = $("#userInput").val();
-    var splitStr = str.split();
-    stringToTranslate.push(splitStr);
-    console.log(stringToTranslate);
-    var vow = ['a','e','i','o','u'];
 
-    var stringToTranslate = stringToTranslate.toString();
+  var inputtedPlaceName = $("input#new-place-name").val();
+  var inputtedCityName = $("input#new-city-name").val();
+  var newPlace = new Contact(inputtedPlaceName, inputtedCityName);
 
+  var inputtedAddress = $("input#new-location-name").val();
+  var inputtedLink = $("input#new-link-name").val();
+  var inputtedSeason = $("input#new-season-name").val();
+  var inputtedReview = $("input#new-review-name").val();
+  var newDescription = new Description(inputtedLink, inputtedSeason, inputtedReview);
 
-    if(vow.indexOf(stringToTranslate.charAt(0)) !== -1) {
-      stringToTranslate += "way";
-    }
-    while(vow.indexOf(stringToTranslate.charAt(0)) === -1) {
-      stringToTranslate = str.substr(1) + str.charAt(0) + "ay";
-    }
+    $("ul#places").append("<li><span class='contact'>" + newPlace.fullPlaceName() + "</span></li>");
 
-    alert(stringToTranslate);
+    $(".contact").last().click(function() {
+      $("#show-places").show();
+      $("#show-places h2").text(newPlace.fullPlaceName());
+      $(".place-name").text(newPlace.placeName);
+      $(".city-name").text(newPlace.cityName);
+      $("ul#descriptions").text("");
+      $("ul#descriptions").append("<li>" + newDescription.fullDescription() + "</li>");
+    });
+
+    resetFields();
+
   });
 });
